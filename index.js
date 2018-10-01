f8d89e4bf2058026840c561f85287f93ab77153f
Macauley Drake <macauleyann@gmail.com>
2018-10-01 17:29:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(numberofblocks){
+  if (numberofblocks<42){
+    return 42-numberofblocks}
+  
+  return numberofblocks-42
+  
+}
+
+function distanceFromHqInFeet(numberofblocks){
+  return distanceFromHqInBlocks(numberofblocks)*264 
+  
+}
